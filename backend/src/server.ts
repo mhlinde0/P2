4d74e24a4ad5126243b586f65b0a2bc2eb03ce89
Marcus <181730171+mhlinde0@users.noreply.github.com>
2025-02-19 13:44:20 +0100

    small change to server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
