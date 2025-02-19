5b2384484a0dd2903401a6a49c75482458019020
pandajakob <jakobm.dev@gmail.com>
2025-02-19 19:21:04 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,7 @@ sudo npm install
 
 sudo tsc
 
-sudo node backend/src/server.js
+# sudo node backend/src/server.js 
+
+#pm2 kører serveren hele tiden og tænder og slukker den selv
+sudo pm2 start backend/src/server.js
