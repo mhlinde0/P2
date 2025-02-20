2361a1de03da17bc1024de651c6e57f5c274c76e
pandajakob <jakobm.dev@gmail.com>
2025-02-20 10:45:48 +0100

    git push

---
+++ backend/src/webhoooks.ts
@@ -0,0 +1,25 @@
+import app from './app';
+
+const { exec } = require("child_process");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.post("/pull-install-deploy", (req, res) => {
+    const payload = req.body;
+
+    if (payload.ref === "refs/heads/main") {
+        console.log("Changes detected on main. Pulling latest changes...");
+
+        exec("sh pull-install-deploy.sh", (err:any, stdout:any, stderr: any) => {
+            if (err) {
+                console.error(`Error: ${stderr}`);
+                return res.status(500).send("Git pull failed");
+            }
+            console.log(`Git Pull Output: ${stdout}`);
+            res.status(200).send("Updated successfully");
+        });
+    } else {
+        res.status(200).send("Not on main, ignoring...");
+    }
+});
