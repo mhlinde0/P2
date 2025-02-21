73654a62efd67f2dd1a8e0601b216a97cfd3e996
pandajakob <jakobm.dev@gmail.com>
2025-02-21 15:01:39 +0100

    ny tekst

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // A simple test route
 app.get('/', (req, res) => {
-  res.send('Velkommen til vores ultra mega seje battleship sænke slagskibe type beat spil.');
+  res.send('BATTLESHIP.');
 });
 
 
