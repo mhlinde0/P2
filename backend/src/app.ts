ae5e8356217d3f0bd02213de5c12aca4bb18ca35
pandajakob <jakobm.dev@gmail.com>
2025-02-21 15:49:00 +0100

    rykket webhooks tilbage i webhook filen

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,32 +8,9 @@ app.use(express.json());
 
 // A simple test route
 app.get('/', (req, res) => {
-  res.send('En eller anden tekst her som viser at det virker');
+  res.send('En eller anden tekst her som viser at det virker!!!');
 });
 
-const { exec } = require("child_process");
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
-
-app.post("/pull-install-deploy", (req, res) => {
-  const payload = req.body;
-  console.log("recieved webhook");
-  if (payload.ref === "refs/heads/main") {
-    console.log("Changes detected on main. Pulling latest changes...");
-
-    exec("sh pull-install-deploy.sh", (err: any, stdout: any, stderr: any) => {
-      if (err) {
-        console.error(`Error: ${stderr}`);
-        return res.status(500).send("Git pull failed");
-      }
-      console.log(`Git Pull Output: ${stdout}`);
-      res.status(200).send("Updated successfully");
-    });
-  } else {
-    res.status(200).send("Not on main, ignoring...");
-  }
-});
 
 
 export default app;
