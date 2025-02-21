3ca780af68cca18eaeff78e097ea2037a73e7e6b
pandajakob <jakobm.dev@gmail.com>
2025-02-21 15:39:08 +0100

    add webhook to server

--- backend/src/webhoooks.ts
+++ backend/src/webhoooks.ts
@@ -1,25 +1,26 @@
-import app from './app';
+    import app from './app';
+    const { exec } = require("child_process");
+    const bodyParser = require("body-parser");
 
-const { exec } = require("child_process");
-const bodyParser = require("body-parser");
+    app.use(bodyParser.json());
 
-app.use(bodyParser.json());
+    app.post("/pull-install-deploy", (req, res) => {
+        const payload = req.body;
+        console.log("recieved webhook")
+        if (payload.ref === "refs/heads/main") {
+            console.log("Changes detected on main. Pulling latest changes...");
 
-app.post("/pull-install-deploy", (req, res) => {
-    const payload = req.body;
-    console.log("recieved webhook")
-    if (payload.ref === "refs/heads/main") {
-        console.log("Changes detected on main. Pulling latest changes...");
+            exec("sh pull-install-deploy.sh", (err:any, stdout:any, stderr: any) => {
+                if (err) {
+                    console.error(`Error: ${stderr}`);
+                    return res.status(500).send("Git pull failed");
+                }
+                console.log(`Git Pull Output: ${stdout}`);
+                res.status(200).send("Updated successfully");
+            });
+        } else {
+            res.status(200).send("Not on main, ignoring...");
+        }
+    });
 
-        exec("sh pull-install-deploy.sh", (err:any, stdout:any, stderr: any) => {
-            if (err) {
-                console.error(`Error: ${stderr}`);
-                return res.status(500).send("Git pull failed");
-            }
-            console.log(`Git Pull Output: ${stdout}`);
-            res.status(200).send("Updated successfully");
-        });
-    } else {
-        res.status(200).send("Not on main, ignoring...");
-    }
-});
+
