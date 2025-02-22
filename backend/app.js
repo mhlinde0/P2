760ca3f213637a99de7c23eaa277f8909475488a
Marcus <181730171+mhlinde0@users.noreply.github.com>
2025-02-22 21:09:23 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
 import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import setupWebhooks from './webhooks.js';
 
 const app = express();
+// Create __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// Middleware to parse JSON bodies
+// Middleware before routes
+app.use(express.static(join(__dirname, '..', 'frontend')));
 app.use(express.json());
 
-// A simple test route
 app.get('/', (req, res) => {
-  res.send('ser du dette virker lortet');
+    res.send('ser du dette virker lortet');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'frontend', 'templates', 'login.html'));
 });
 
 // Initialize webhooks AFTER app is created
