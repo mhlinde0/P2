1abb03b36deb9c8b8f13b5824d0060d7a59f4dab
Marcus <181730171+mhlinde0@users.noreply.github.com>
2025-02-24 16:12:03 +0100

    see changes under:
    - changed from require to import (es modules)
    - created api folder for userroutes
    - removed userroutes from server, added to app
    - fixed html static site routing in frontend

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,17 @@
-import connectDB from './config/db.js';    //db.js function(mongoose)
-import app from './app.js';
-import express from "express";
+import connectDB from "./config/db.js";
+import app from "./app.js";
 
-//----------------CONSTANTS----------------//
 const PORT = process.env.PORT || 4000;
-const userRoutes = require('./routes/userRoutes.js')
-const mongoose = require('mongoose')
-//----------------------------------------//
-
-
-
-//********MiddleWare********//
-app.use(express.json())
-//==========================//
-
-
-
-//**********Routes**********//
-app.use('/api/userRoutes',userRoutes)
-//==========================//
-
 
 (async () => {
-  await connectDB(); //awaits for DB connection before server start
+  await connectDB(); // Wait for DB connection before starting the server
 
-  //request listener
+  // Start the server
   app.listen(PORT, () => {
     console.log(`Server is running on port localhost:${PORT}`);
   });
 })();
+
 
 
 
