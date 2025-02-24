7faf723e2b656db2535226dfd400431610a72e1f
Jakob Michaelsen <jakobm.dev@gmail.com>
2025-02-24 10:41:18 +0100

    tilføjet lyd og musik (#25)
    Har tilføjet nogle lyde, som bliver spillet ved hjælp af "howler". Alle lydene kan findes under resources/sounds, og man sætter dem til at virke inde på 'audio manager'

---
+++ frontend/scripts/audioManager.js
@@ -0,0 +1,17 @@
+var click = new Howl({
+    src: ["../resources/sounds/mouse_click.wav"],
+    volume: 0.1,
+});
+
+var music = new Howl({
+    src: ["../resources/sounds/music1.mp3"],
+    volume: 0.5,
+    loop: true,
+    autoplay: true
+})
+
+document.addEventListener("click", () => {
+    click.play();
+});
+
+
