d6fd0cc3f772cf6051ce3a3d74017b6137e4816d
asbjorngosvig <asbjorn.gosvig@gmail.com>
2025-02-28 17:35:08 +0100

    Har delt funktionen op i flere dele hvor de bliver kaldt fra onShipDrop funktionen som Jakob foreslog

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -32,8 +32,9 @@ function createBoards() {
 
 }
 
-// Gets array of ship divs
+// Array of ship divs
 const shipsDiv = document.querySelectorAll(".ship");
+// Array of squares that are filled by ships
 const occupiedSquareArray = [];
 
 // Creates 100 squares to fill the game boards and adds drag and drop functionalty to them
@@ -47,85 +48,102 @@ function createSquares(gameboard) {
             event.preventDefault();
         })
 
-        square.addEventListener("drop", (event) => {
-            event.preventDefault();
-
-            const shipId = event.dataTransfer.getData("text/plain");
-            const draggedShipElement = document.getElementById(shipId);
-            let draggedShip = null;
-
-            if (draggedShipElement.id === "destroyerSize2") {
-                draggedShip = destroyer;
-            } else if (draggedShipElement.id === "submarineSize3") {
-                draggedShip = submarine;
-            } else if (draggedShipElement.id === "cruiserSize3") {
-                draggedShip = cruiser;
-            } else if (draggedShipElement.id === "battleshipSize4") {
-                draggedShip = battleship
-            } else if (draggedShipElement.id === "carrierSize5") {
-                draggedShip = carrier;
-            }
-
-            const droppedSquare = parseInt(square.id.replace("square", ""), 10);
-
-            const draggedShipLength = draggedShip.length;
-            const draggedShipRotation = parseInt(draggedShipElement.getAttribute("data-rotation") || "0", 10);
-
-            let coveredSquares = [];
-
-            const squareNumberStart = droppedSquare
-            const startColumn = (squareNumberStart -1) % width;
-            const startRow =  Math.floor((squareNumberStart -1) / width);
-
-            if (draggedShipRotation % 180 === 0) {
-                if (startRow + draggedShipLength > height) {
-                    alert("Ship is out of bounds.");
-                    return;
-                }
-
-                for (let j = 0; j < draggedShipLength; j++) {
-                    let droppedSquareIndex = droppedSquare + j * width;
-                    const squareElement = document.getElementById("square" + droppedSquareIndex);
-                    if (occupiedSquareArray.includes(squareElement)) {
-                        alert("Ship overlaps another ship.");
-                        return;
-                    }
-                    coveredSquares.push(droppedSquare + j * width);
-                    document.getElementById(draggedShipElement.id).style.display = "none";
-                    }
-                }
-              else {
-                if (startColumn + draggedShipLength > height) {
-                    alert("Ship is out of bounds.");
-                    return;
-                }
-                for (j = 0; j < draggedShipLength; j++) {
-                    let droppedSquareIndex = droppedSquare + j;
-                    const squareElement = document.getElementById("square" + droppedSquareIndex);
-                    if (occupiedSquareArray.includes(squareElement)) {
-                        alert("Ship overlaps another ship.");
-                        return;
-                    }
-                    coveredSquares.push(droppedSquare + j);
-                    document.getElementById(draggedShipElement.id).style.display = "none";
-                }
-            }
-
-            coveredSquares.forEach(index => {
-                const occupiedSquare = document.getElementById("square" + index);
-                if (occupiedSquare) {
-                    occupiedSquare.classList.add("occupiedSquare");
-                    occupiedSquareArray.push(occupiedSquare)
-                }
-            })
-
-            console.log(occupiedSquareArray);
-
-        })
+        square.addEventListener("drop", onShipDrop);
 
         gameboard.append(square);
     }
 }
+
+function onShipDrop(event) {
+    event.preventDefault();
+
+    const square = event.currentTarget;
+    const shipId = event.dataTransfer.getData("text/plain"); // text/plain fortæller at dataen vi leder efter er ren tekst
+    const draggedShipElement = document.getElementById(shipId);
+    let draggedShip = null;
+
+    // finder hvilken class ship vi skal bruge ud fra html elementet
+    if (draggedShipElement.id === "destroyerSize2") {
+        draggedShip = destroyer;
+    } else if (draggedShipElement.id === "submarineSize3") {
+        draggedShip = submarine;
+    } else if (draggedShipElement.id === "cruiserSize3") {
+        draggedShip = cruiser;
+    } else if (draggedShipElement.id === "battleshipSize4") {
+        draggedShip = battleship;
+    } else if (draggedShipElement.id === "carrierSize5") {
+        draggedShip = carrier;
+    }
+
+    const droppedSquare = parseInt(square.id.replace("square", ""), 10); // Finder firkantens tal ved at fjerne "square" og give resten som en int
+    const draggedShipLength = draggedShip.length;
+    const draggedShipRotation = parseInt(draggedShipElement.getAttribute("data-rotation") || "0", 10); // Finder skibets nuværende rotation ved at finde attributen "data-rotation" og give den som en int
+
+    const startColumn = (droppedSquare - 1) % width;
+    const startRow = Math.floor((droppedSquare - 1) / width);
+
+    let coveredSquares = [];
+
+    if (draggedShipRotation % 180 === 0) { // Hvis % 180 === 0 er sandt betyder det at skibet er lodret 
+        if (checkForOutOfBounds(startRow, startColumn, draggedShipLength, draggedShipRotation)) {
+            alert("Ship is out of bounds.");
+            return;
+        }
+        for (let j = 0; j < draggedShipLength; j++) { // Hvis skibet kan være der bliver felterne placeret i arrayet
+            coveredSquares.push(droppedSquare + j * width);
+        }
+    } else { // Hvis det ikke er sandt % 180 === 0 betyder det at skibet er vandret
+        if (checkForOutOfBounds(startRow, startColumn, draggedShipLength, draggedShipRotation)) {
+            alert("Ship is out of bounds.");
+            return;
+        }
+        for (let j = 0; j < draggedShipLength; j++) {
+            coveredSquares.push(droppedSquare + j);
+        }
+    }
+
+    if (checkForOverlap(coveredSquares)) {
+        alert("Ship overlaps another ship.");
+        return;
+    }
+
+    draggedShipElement.style.display = "none"; // Gør html elementet usynligt når skibet bliver placeret
+
+    assignOccupiedSquares(coveredSquares);
+
+    console.log(occupiedSquareArray);
+}
+
+function checkForOutOfBounds(startRow, startColumn, shipLength, rotation) {
+    if (rotation % 180 === 0) { // Hvis lodret
+        return (startRow + shipLength > height);
+    } else { // Hvis vandret
+        return (startColumn + shipLength > width);
+    }
+}
+
+// Tjekker om der allerede befinder sig et skib på de ønskede squares
+function checkForOverlap(coveredSquares) {
+    for (let i = 0; i < coveredSquares.length; i++) {
+        const squareElement = document.getElementById("square" + coveredSquares[i]);
+        if (occupiedSquareArray.includes(squareElement)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Tilføjer elementet occupiedSquare til de squares med skibe på
+function assignOccupiedSquares(coveredSquares) {
+    coveredSquares.forEach(index => {
+        const squareElement = document.getElementById("square" + index);
+        if (squareElement) {
+            squareElement.classList.add("occupiedSquare");
+            occupiedSquareArray.push(squareElement);
+        }
+    });
+}
+
 
 shipsDiv.forEach(ship => {
     ship.addEventListener("dragstart", (event) => {
