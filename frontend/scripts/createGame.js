1acda2503575c336a10792350e2cf762d478cc96
pandajakob <jakobm.dev@gmail.com>
2025-02-28 19:24:40 +0100

    game id generator

---
+++ frontend/scripts/createGame.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById('gameId').innerHTML = "GAME ID: " + generateGameId();
+
+
+});
+
+// Generates random 6 character ID
+function generateGameId() {
+    let gameId = ""
+
+    for (let i = 0; i < 6; i++) {
+        if (Math.random()<0.5) {
+            const randomLetterCode = Math.floor(Math.random()*25)+65; 
+            gameId += String.fromCharCode(randomLetterCode) 
+        } else {
+            gameId += String(Math.floor(Math.random()*9))
+        }   
+    }
+    return gameId;
+}
