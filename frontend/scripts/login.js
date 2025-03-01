185009425a179c0efe11b9a86e67f7b99273e48f
pandajakob <jakobm.dev@gmail.com>
2025-03-01 16:11:59 +0100

    login and out

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,28 +1,23 @@
 import { setCookie, getCookie } from './cookies.js';
-import { isLoading, setUser, getUser } from './state.js';
+import { isLoading, setUser, getUser, setIsLoggedIn } from './state.js';
 
 const loginForm = document.getElementById("loginForm");
 const rememberMeBox = document.getElementById("rememberMe");
 
-const username = document.getElementById("username").value
-const password = document.getElementById("password").value
+document.addEventListener("DOMContentLoaded", rememberMe)
 
-
-document.addEventListener("DOMContentLoaded", (e) => {
+function rememberMe(e) {
     e.preventDefault();
     if (getCookie("rememberMe") === "true") {
         document.getElementById("username").value = getCookie("username");
         document.getElementById("password").value = getCookie("password");
-
         rememberMeBox.checked = true;
     }
-})
+}
 
-
-loginForm?.addEventListener("submit", function (event) {
-    event?.preventDefault();
-
-
+function setRememberMeCookies() {
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
     if (rememberMeBox.checked) {
         setCookie("username", username, 5);
         setCookie("password", password, 5);
@@ -32,21 +27,31 @@ loginForm?.addEventListener("submit", function (event) {
         setCookie("password", "", -1);
         setCookie("rememberMe", "", -1);
     }
+}
 
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    login();
 })
 
-
 async function login() {
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
     try {
         isLoading(true);
 
         // API CALL TO LOGIN
 
-
         //Update frontend userState
 
-        setUser(user);
-
+        setUser(username);
+        console.log("user", getUser())
+        if (getUser()) {
+            setIsLoggedIn(true);
+            console.log("is signed in")
+        }
+        setRememberMeCookies();
+        window.location.href = "/"
     }
     catch (err) {
         console.log(err);
