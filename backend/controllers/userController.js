5ae49dfe10ae79e30e8cd12aba1a712f16932748
RyanZachariasen <143599102+RyanZachariasen@users.noreply.github.com>
2025-03-04 20:44:48 +0100

    Add functional post method in userRoutes.js
    Add get method for users
    
    Add all routes
    Add controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,62 @@
+import User from "../models/User.js";
+import mongoose from "mongoose";
+
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({});//empty means all
+        res.status(200).json({success: true, data: users})
+
+    }catch (error){
+        console.log("error fetching users :", error.message);
+        res.status(500).json({success:false, message: "Server error"});
+    }}
+
+export const getUser = async (req, res) => {
+    const { id } = req.params;
+    console.log("id: ",id); //debugging to see in terminal
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid User Id" });
+    }
+
+    try{
+        await User.findById(id);
+        res.status(200).json({success: true, message: "User found"});
+    }catch(error){
+        res.status(404).json({success: false, message: "User not found"});
+    }
+}
+
+export const createUser = async (req, res) => { //async so we can call await
+    const user = req.body; //user sending data
+
+    if(!user.name || !user.email || !user.password){//middleware
+        return res.status(400).json({success:false, message: "Provide all fields"});
+    }
+    const newUser = new User(user);
+
+    try{
+        await newUser.save();
+        res.status(201).json({success: true, data: newUser});//201 means something created
+
+    }catch(error){
+        console.log("Error in creating user :", error.message);
+        res.status(500).json({success: false, message: "Server error"});
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    console.log("id: ",id); //debugging to see in terminal
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid User Id" });
+    }
+    try{
+        await User.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "User deleted"});
+    }catch(error){
+        res.status(500).json({success: false, message: "Server Error"});
+    }
+
+}
--- backend/routes/api/userRoutes.js
+++ backend/routes/api/userRoutes.js
@@ -1,56 +1,30 @@
 import express from "express";
-import User from "../../models/User.js";
 
+import {getAllUsers, getUser, createUser, deleteUser} from "../../controllers/userController.js";
 
-const app = express();
+//const app = express();
+
 const userRoutes = express.Router();
-app.use(express.json()); //json data in body -middleware
+
+//app.use(express.json()); //json data in body -middleware
+
 
 // Get all users
-userRoutes.get("/users",async (req, res) => {
-  try {
-    const users = await User.find({});//empty means all
-    res.status(200).json({success: true, data: users})
-
-  }catch (error){
-    console.log("error fetching users :", error.message);
-    res.status(500).json({success:false, message: "Server error"});
-  }
-});
+userRoutes.get("/", getAllUsers);
 
 // Get a single user
-userRoutes.get("/:id", (req, res) => {
-  res.json({ mssg: "GET SINGLE USER" }); // Dummy
-});
+userRoutes.get("/:id", getUser);
 
 // POST a new user
-userRoutes.post("/users", async (req, res) => { //async so we can call await
-  const user = req.body; //user sending data
-
-  if(!user.name || !user.email || !user.password){//middleware
-  return res.status(400).json({success:false, message: "Provide all fields"});
-  }
-  const newUser = new User(user);
-
-  try{
-  await newUser.save();
-  res.status(201).json({success: true, data: newUser});//201 means something created
-
-  }catch(error){
-  console.log("Error in creating user :", error.message);
-  res.status(500).json({success: false, message: "Server error"});
-  }
-});
-
+userRoutes.post("/", createUser);
 
 // Delete a user
-userRoutes.delete("/:id", (req, res) => {
-  res.json({ mssg: "delete user" }); // Dummy
-});
+userRoutes.delete("/:id", deleteUser);
 
+/*
 // Update a user
 userRoutes.put("/:id", (req, res) => {
   res.json({ mssg: "update user" }); // Dummy
 });
-
+*/
 export default userRoutes;
