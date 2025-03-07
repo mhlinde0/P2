4fd54baa0ec0e06c563eba574dc6bdd52f100b85
pandajakob <jakobm.dev@gmail.com>
2025-03-07 15:24:25 +0100

    remove generate user id

--- frontend/scripts/register.js
+++ frontend/scripts/register.js
@@ -48,27 +48,4 @@ async function registerUser() {
     isLoading(false);
 }
 
-/* Generates size 10 userId, with a-Z and 1-9*/
-function generateUserId() {
-    let userId = ""
-    let characters = [];
 
-    for (let i = 1; i <= 9; i++) {
-        characters.push(String(i))
-    }
-
-    // gets all char from a-Z
-    for (let i = 65; i <= 122; i++) {
-        if (i < 91 || i > 96) { // skip "[, \/, ^," ...
-            characters.push(String.fromCharCode(i))
-        }
-    }
-
-    for (let i = 0; i < 10; i++) {
-        const length = characters.length;
-        userId += characters[Math.floor(Math.random() * length)]
-    }
-    return userId;
-}
-
-export { generateUserId }
