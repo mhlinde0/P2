8ee7d4d4ba82cf05d1cc0a82bec0c29eb801dc1b
Marcus <181730171+mhlinde0@users.noreply.github.com>
2025-03-10 09:12:43 +0100

    unessecary naming fixed

--- backend/routes/staticRoutes.js
+++ backend/routes/staticRoutes.js
@@ -2,43 +2,43 @@ import { Router } from 'express';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const staticRoutes = Router();
+const router = Router();
 
 // Create __dirname equivalent in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Define your routes
-staticRoutes.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.sendFile(join(__dirname, '..', '..', 'frontend', 'index.html'));
 });
 
-staticRoutes.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
     res.sendFile(join(__dirname, '..', '..', 'frontend', 'templates', 'login.html'));
 });
 
-staticRoutes.get('/register', (req, res) => {
+router.get('/register', (req, res) => {
     res.sendFile(join(__dirname, '..', '..', 'frontend', 'templates', 'register.html'));
 });
 
-staticRoutes.get('/joinGame', (req, res) => {
+router.get('/joinGame', (req, res) => {
     res.sendFile(join(__dirname, '..', '..', 'frontend', 'templates', 'joinGame.html'));
 });
 
-staticRoutes.get('/createGame', (req, res) => {
+router.get('/createGame', (req, res) => {
     res.sendFile(join(__dirname, '..', '..', 'frontend', 'templates', 'createGame.html'));
 });
 
-staticRoutes.get('/profile', (req, res) => {
+router.get('/profile', (req, res) => {
     res.sendFile(join(__dirname, '..', '..', 'frontend', 'templates', 'profile.html'));
 });
 
-staticRoutes.get('/settings', (req, res) => {
+router.get('/settings', (req, res) => {
     res.sendFile(join(__dirname, '..', '..', 'frontend', 'templates', 'settings.html'));
 });
 
-staticRoutes.get('/game:id', (req, res) => {
+router.get('/game:id', (req, res) => {
     res.sendFile(join(__dirname, '..', '..', 'frontend', 'templates', 'game.html'));
 });
 
-export default staticRoutes;
+export default router;
