2f6dd4518abd8e1e160853031739a26bfb07f18e
pandajakob <jakobm.dev@gmail.com>
2025-03-19 09:39:37 +0100

    fix small errors

--- test/unit.test.js
+++ test/unit.test.js
@@ -1,23 +1,20 @@
-import {generateBattleNumber } from "../frontend/scripts/createGame";
-import "../frontend/scripts/login";
-//import "../frontend/scripts/register";
-// import "../frontend/scripts/cookies";
-////import "../frontend/scripts/game.js";
-//import "../frontend/scripts/settings.js";
-//import "../frontend/scripts/state.js";
-import { assert, expect, test, it} from "vitest";
+import { assert, expect, test, it } from "vitest";
 
-// Test cases must be wrapped in a test function
-test("Generates size 6 battle number with correct chars", () => {
-    const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    let battleNumber = generateBattleNumber();
-    expect(battleNumber.length).toBe(6);
 
-    // run test 1000 times
-    for (let i = 0; i < 1000; i++) {
-        for(let i = 0; i<6; i++) {
-            assert(char.includes(battleNumber[i]))
-        }
-    }
+test("User creation test", () => {
+
 });
+
+test("User retrieval test", () => {
+
+});
+
+test("User modification test", () => {
+
+});
+
+test("User deletion test", () => {
+
+});
+
 
--- vitest.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    environment: "jsdom", // Use jsdom to enable `localStorage` and other browser APIs
-  },
-});
